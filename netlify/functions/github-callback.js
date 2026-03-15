f0f27e86a699e829d3a5431870f3b2826be14f04
IFraimG <kulsasha55@gmail.com>
2026-03-15 16:02:03 +0300

    update

--- netlify/functions/github-callback.js
+++ netlify/functions/github-callback.js
@@ -1,62 +1,35 @@
-// export async function handler(event) {
-
-//     const code = event.queryStringParameters.code
-  
-//     const response = await fetch("https://github.com/login/oauth/access_token", {
-//       method: "POST",
-//       headers: {
-//         "Accept": "application/json",
-//         "Content-Type": "application/json"
-//       },
-//       body: JSON.stringify({
-//         client_id: process.env.GITHUB_CLIENT_ID,
-//         client_secret: process.env.GITHUB_CLIENT_SECRET,
-//         code
-//       })
-//     })
-  
-//     const data = await response.json()
-//     console.log(data)
-  
-//     if (!data.access_token) {
-//       return {
-//         statusCode: 302,
-//         headers: { Location: "/" }
-//       }
-//     }
-  
-//     return {
-//       statusCode: 302,
-//       headers: {
-//         "Set-Cookie": `github_token=${data.access_token}; HttpOnly; Secure; SameSite=Lax; Path=/`,
-//         Location: "/dashboard"
-//       }
-//     }
-//   }
-
 export async function handler(event) {
 
-  const code = event.queryStringParameters.code
-
-  const response = await fetch(
-    "https://github.com/login/oauth/access_token",
-    {
+    const code = event.queryStringParameters.code
+  
+    const response = await fetch("https://github.com/login/oauth/access_token", {
       method: "POST",
       headers: {
-        Accept: "application/json"
+        "Accept": "application/json",
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
         code
       })
+    })
+  
+    const data = await response.json()
+    console.log(data)
+  
+    if (!data.access_token) {
+      return {
+        statusCode: 302,
+        headers: { Location: "/" }
+      }
     }
-  );
-
-  const data = await response.json();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data)
-  };
-};
+  
+    return {
+      statusCode: 302,
+      headers: {
+        "Set-Cookie": `github_token=${data.access_token}; HttpOnly; Secure; SameSite=Lax; Path=/`,
+        Location: "/dashboard"
+      }
+    }
+  }
