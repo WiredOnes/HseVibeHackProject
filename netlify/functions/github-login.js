9ae376fd0fd8e241e6d2c75e8f23d3667ab99665
IFraimG <kulsasha55@gmail.com>
2026-03-15 14:24:10 +0300

    update

--- netlify/functions/github-login.js
+++ netlify/functions/github-login.js
@@ -3,7 +3,7 @@ export async function handler() {
     const params = new URLSearchParams({
       client_id: process.env.GITHUB_CLIENT_ID,
       scope: "read:user user:email",
-      redirect_uri: `https://hsehackvibeproject.netlify.app/.netlify/functions/github-callback`
+      redirect_uri: "https://hsehackvibeproject.netlify.app/.netlify/functions/github-callback"
     })
   
     return {
